9fdbe11538e1421d2441726be813439d51431846
ValeraVasilevsky <valera200208@gmail.com>
2025-01-17 15:36:39 +0700

    fix: some styles

--- src/pages/main/index.vue
+++ src/pages/main/index.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>main zxczxczxc</div>
+  <div :class="styles.container">
+    <Placeholder text="Выберите сотрудника, чтобы посмотреть его профиль" />
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Placeholder } from "shared/ui/placeholder";
+
+import styles from "./styles.module.scss";
+</script>
--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,3 +1,4 @@
 export * from "./main-header";
 export * from "./typography";
 export * from "./base-input";
+export * from "./placeholder";
