114265960ca3c9e1b10d15097bdd50c6d080032c
ValeraVasilevsky <valera200208@gmail.com>
2025-01-17 14:43:50 +0700

    feat: remove useless code, created header component and styled layout

--- src/pages/layout.vue
+++ src/pages/layout.vue
@@ -1,9 +1,23 @@
 <template>
-  <RouterView v-slot="{ Component }">
-    <Suspense>
-      <template #default>
-        <component :is="Component" />
-      </template>
-    </Suspense>
-  </RouterView>
+  <div :class="styles.container">
+    <MainHeader />
+
+    <div :class="styles.wrapper">
+      <div :class="styles.page">
+        <RouterView v-slot="{ Component }">
+          <Suspense>
+            <template #default>
+              <component :is="Component" />
+            </template>
+          </Suspense>
+        </RouterView>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { MainHeader } from "shared/ui";
+
+import styles from "./styles.module.scss";
+</script>
---
+++ src/shared/ui/main-header/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <header :class="styles.header">
+    <Typography variant="font-xl" :class="styles.logo">Жилфонд</Typography>
+    <Typography variant="font-m" :class="styles.user">Пользователь</Typography>
+  </header>
+</template>
+
+<script setup lang="ts">
+import { Typography } from "../typography";
+
+import styles from "./styles.module.scss";
+</script>
